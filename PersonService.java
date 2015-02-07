eb347cb89f928afdb19bef4e33948e4d7dab220e
mohammadgholeh <mohammad_gholeh@hotmail.com>
2015-02-07 15:28:55 +0200

    fixed merge

--- PersonService.java
+++ PersonService.java
@@ -4,17 +4,12 @@ public class PersoneService{
 	public PersoneService(Persons personeObject){
 		this.personeObject=personeObject;
 	}
-<<<<<<< HEAD
 	public void toString(){
 		System.out.print();
 	}
-=======
 
 	public Person getPerson(){
 		return personeObject;
 	}
-
-
->>>>>>> issu3
 
 }
